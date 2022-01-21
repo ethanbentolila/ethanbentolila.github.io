9886afefc2be9abdcd7beb6e42a5d561603cbf40
Ethan <ethanbentolila@gmail.com>
2022-01-21 15:32:48 -0500

    updated live server with changes to ICE2

--- Scripts/app.js
+++ Scripts/app.js
@@ -12,6 +12,44 @@
             //redirects to about page 
             location.href = "about.html";
         });
+
+    
+
+
+        //Step 1 get reference to an entry point(s) (insertion point/ deletion point)
+        let Maincontent = document.getElementsByTagName("main")[0];
+        let DocumentBody = document.body;
+
+        //Step 2 create an element(s) to insert
+        let MainParagraph = document.createElement("p");
+        let Article = document.createElement("article");
+        let ArticleParagraph = `<p id="ArticleParagraph" class=""mt-3">This is the article paragraph</p>`;
+
+        //Step 3 configure new element
+        MainParagraph.setAttribute("id", "Main Paragraph");
+        MainParagraph.setAttribute("class", "mt-3");
+
+        let FirstParagraphString = "This is";
+        //Example of Template string
+        let SecondParagraphString = `${FirstParagraphString} the Main Paragraph`;
+
+        MainParagraph.textContent = SecondParagraphString;
+        Article.setAttribute("class","container");
+
+        //Step 4 add / insert new element
+        Maincontent.appendChild(MainParagraph);
+        Article.innerHTML = ArticleParagraph;
+        DocumentBody.appendChild(Article);
+
+        //Deletion example
+        //document.getElementById("ArticleParagraph").remove();
+        
+        //Insert Before example
+        let NewH1 = document.createElement("h1");
+        NewH1.setAttribute("class","display-1");
+        NewH1.textContent = "Hello, World!";
+        Maincontent.before(NewH1);
+
     }
 
     //Named function
